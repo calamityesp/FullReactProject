74dd8b28ddf514b2d946d3fc64a5df268d64a1b3
Keylan Shannon <keylan.d.shannon@hotmail.com>
2021-06-15 20:37:19 -0600

    More advanced code

--- src/components/menu-item/main-item.component.jsx
+++ src/components/menu-item/main-item.component.jsx
@@ -2,15 +2,16 @@ import "./menu-item.styles.scss";
 import React from "react";
 
 const MenuItem = ({ title, imageUrl, size }) => (
-  <div
-    className={`menu-item ${size}`}
-    style={{
-      //allows dynamic styling which is awesome
-      backgroundImage: `url(${imageUrl})`,
-    }}
-  >
+  <div className={`menu-item ${size}`}>
+    <div
+      className="background-image"
+      style={{
+        //allows dynamic styling which is awesome
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
     <div className="content">
-      <h1 className="title">{title}</h1>
+      <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
